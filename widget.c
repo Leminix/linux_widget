79555f7abddffe42f1269a6ace8adafeab25cd62
Filip <f.jaros05@seznam.cz>
2025-06-10 19:20:34 +0200

    open and close device

--- widget.c
+++ widget.c
@@ -1,10 +1,32 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <errno.h>
+#include <fcntl.h>
 
 
-void create_connection(){
-    printf("hello world\n");
+
+int create_connection(){
+    
+    int fb = open("/dev/fb0", O_RDWR);  // use open syscall to open fb0 device in path /dev
+    if(fb < 0){                         // open it for read and write
+        perror("ERROR");                // if the fb is less then 0 print an ERROR message
+        return errno;
+    }
+
+
+    // write a code which will get time and date from OS
+    // write a code which will call function to convert data to binary code
+
+
+    if(close(fb) < 0){          // close device and check returned value
+        perror("ERROR");        // if a value is less then 0 print an ERROR message
+        return errno;           // but if the value is 0 return 0 to main function
+    }
+
+    return 0;
+
 }
+
 
 
 int main(void){
